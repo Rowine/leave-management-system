5ae81307dcb3923748821a3457072e3872f8a2f1
Rowine <rowine12345@gmail.com>
2023-03-29 09:16:46 +0800

    Created user endpoints

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,25 @@ import chalk from 'chalk'
 import dotenv from 'dotenv'
 import express from 'express'
 import connectDB from './config/db.js'
+import { errorHandler, notFound } from './middleware/errorMiddleware.js'
+import userRoutes from './routes/userRoutes.js'
+// import {} from './routes/leaveRoutes.js'
+// import {} from './routes/categoryRoutes.js'
 
 dotenv.config()
 
 connectDB()
 
 const app = express()
+
+app.use(express.json())
+
+app.use('/api/users', userRoutes)
+// app.use('/api/leaves', leaveRoutes)
+// app.use('/api/categories', categoryRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
